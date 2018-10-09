d29401eeef44429d8e2f1dfbecb9704dc7f0ce0d
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-09 01:32:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let store = { drivers: [], passengers: [] };
+
+let driverId = 0;
+
+class Driver{
+    constructor(name) {
+       this.id = ++driverId;
+       this.name = name;
+        store.drivers.push(this);
+    }
+}
+let passengerId = 0;
+
+class Passenger{
+    constructor(name) {
+       this.id = ++passengerId;
+       this.name = name;
+        store.passengers.push(this);
+
+    }
+}
+
+let tripId = 0;
+
+
+class Trip{
+    constructor(driver, passenger) {
+       this.id = ++tripId;
+       store.trips.push(this);
+
+
+    }
+}
